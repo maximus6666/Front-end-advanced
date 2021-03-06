08b7e3a29fadbbf4c4a028da4afd862f3684c331
Maksym Dmytryshyn <dmytryshyn_max@ukr.net>
2021-03-06 14:23:26 +0200

    Add new functions

--- 5-arrays/script.js
+++ 5-arrays/script.js
@@ -1,43 +1,25 @@
 //1 Функція, яка повертає масив випадкових цілих чисел
 function getRandomArray(length, min, max) {
-  let result = [];
+  let result = Array.from({
+    length
+  });
 
-  for (let i = 0; i < length; i++) {
-    const randomNum = Math.floor(Math.random() * (max - min + 1) + min);
-    result.push(randomNum);
-  }
-
-  return result;
+  return result.map((number) => Math.floor(Math.random() * (max - min + 1) + min));
 }
 
 //2 Функція, яка рахує середнє арифметичне
 function getAverage(...numbers) {
-  const integerArr = [];
-
-  numbers.forEach((number) => {
-    if (Number.isInteger(number)) {
-      integerArr.push(number);
-    }
-  });
-
+  const integerArr = numbers.filter((number) => Number.isInteger(number));
   const sum = integerArr.reduce((sum, number) => {
     return sum + number;
   }, 0);
 
   return sum / integerArr.length;
-
 }
 
 //3 Функція, яка визначає медіану
 function getMedian(...numbers) {
-  const integerArr = [];
-
-  numbers.forEach((number) => {
-    if (Number.isInteger(number)) {
-      integerArr.push(number);
-    }
-  });
-
+  const integerArr = numbers.filter((number) => Number.isInteger(number));
   integerArr.sort((a, b) => a - b);
 
   const isNotEvenLength = integerArr.length % 2;
@@ -72,17 +54,67 @@ function countPositiveNumbers(...numbers) {
 //та залишить тільки ті, які діляться на ціло на 5
 function getDividedByFive(...numbers) {
   const result = numbers.filter((number) => {
-    return !Boolean(number % 5);
+    return number % 5 === 0;
   });
 
   return result;
 }
 
+//7 Функція заміняє погані слова на зірочки (*)
+let badWordsBank = ['fuck', 'shit'];
 
+function replaceBadWords(string, ...badWords) {
+  if (badWords) {
+    badWordsBank = badWordsBank.concat(badWords);
+  }
 
-//console.log('1.', getRandomArray(15, 0, 20));
-//console.log('2.', getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
-//console.log('3.', getMedian(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3));
-//console.log('4.', filterEvenNumbers(1, 2, 2, 5, 3, 4, 5, 6));
-//console.log('5.', countPositiveNumbers(1, -2, 3, -4, -5, 6, 0, -8));
-//console.log('6.', getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
+  const arrOfWords = string.split(' ');
+
+  const censoredArr = arrOfWords.map((word) => {
+    const currentWord = word.toLowerCase();
+    let censoredWord = '';
+
+    badWordsBank.forEach((badWord) => {
+      if (currentWord.includes(badWord)) {
+        const star = '*'.repeat(badWord.length);
+        censoredWord = currentWord.replaceAll(badWord, star);
+      }
+    });
+
+    return censoredWord || word;
+  });
+
+  return censoredArr.join(' ');
+}
+
+//8. Функція яка розбиває кожне слово на умовні склади по 3 букви.
+function divideByThree(word) {
+  const arrOfWord = word.replaceAll(' ', '').toLowerCase().split('');
+  const dividedArray = arrOfWord.reduce(((result, curentLetter) => {
+    if (result.length === 0) {
+      result.push(curentLetter);
+
+    } else {
+      let lastLetter = result.length - 1;
+
+      if (result[lastLetter].length === 3) {
+        result.push(curentLetter);
+      } else {
+        result[lastLetter] += curentLetter;
+      }
+    }
+
+    return result;
+  }), []);
+
+  return dividedArray;
+}
+
+console.log('1.', getRandomArray(15, 0, 20));
+console.log('2.', getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
+console.log('3.', getMedian(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
+console.log('4.', filterEvenNumbers(1, 2, 2, 5, 3, 4, 5, 6));
+console.log('5.', countPositiveNumbers(1, -2, 3, -4, -5, 6, 0, -8));
+console.log('6.', getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
+console.log('7.', replaceBadWords("Are you fucking kidding?"));
+console.log('8', divideByThree('Commander'));
